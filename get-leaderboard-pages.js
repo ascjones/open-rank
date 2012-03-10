086ae5e9acef64abe70d9a0028c8c926007d2017
Andrew Jones <ascjones@gmail.com>
2012-03-11 00:02:49 +0200

    emit leaderboard pages at the end

--- get-leaderboard-pages.js
+++ get-leaderboard-pages.js
@@ -4,14 +4,14 @@ exports.job = new nodeio.Job({
   run: function () {
     var leaderboardRootUrl = "http://leaderboard.crossfit.com";
     var firstLeaderboardPage = leaderboardRootUrl + "/leaderboard.php?stage=0&sort=0&did=1&rid=1&npp=100&p=0&dd=1"
-    var that = this;
     this.getHtml(firstLeaderboardPage, function(err, $) {
       if (err) this.exit(err);
-      this.emit(firstLeaderboardPage);
+      var leaderboardPages = [firstLeaderboardPage];
       $('#leaderboard-pager a').each(function(page) {
         var leaderboardUrl = leaderboardRootUrl + page.attribs.href;
-        that.emit(leaderboardUrl);
+        leaderboardPages.push(leaderboardUrl);
       });
+      this.emit(leaderboardPages);
     });
   }
 });
