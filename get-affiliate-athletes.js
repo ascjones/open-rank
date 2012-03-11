5ed266caeca72e23be077554f68a5828e3059172
Andrew Jones <ascjones@gmail.com>
2012-03-11 20:46:54 +0200

    get athlete ids that belong to an affiliate

--- get-affiliate-athletes.js
+++ get-affiliate-athletes.js
@@ -2,14 +2,16 @@ var nodeio = require('node.io');
 exports.job = new nodeio.Job({
   input: ['http://games.crossfit.com/affiliate/2132'],  
   run: function (affiliateUrl) {
+    var parseAthleteId = /\d+/;
     this.getHtml(affiliateUrl, function(err, $) {
       if (err) this.exit(err);
-      var affiliates = [];
+      var athletes = [];
       $('section.block-affiliate-athletes ul li').each(function(li) {
-        var athleteLink = li.children[1];
-        affiliates.push(athleteLink.attribs.href);
-      });      
-      this.emit(affiliates);
-    });
+        var athleteLink = $('a', li).first().attribs.href;
+        var athleteId = parseAthleteId.exec(athleteLink)[0];
+        athletes.push(athleteId);
+      });     
+      this.emit(athletes);
+    })
   }
-});
+})
