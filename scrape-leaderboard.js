addef1027eca83605f5f78439c2f0f1ede7e7966
Andrew Jones <ascjones@gmail.com>
2012-03-11 15:51:33 +0200

    get rank and athlete url

--- scrape-leaderboard.js
+++ scrape-leaderboard.js
@@ -1,23 +1,22 @@
 var nodeio = require('node.io');
 exports.job = new nodeio.Job({
-  input: false,
-  run: function () {
-    var url = "http://leaderboard.crossfit.com/leaderboard.php?stage=0&sort=0&did=1&rid=1&npp=100&p=0&dd=1"
-    this.getHtml(url, function(err, $) {
+  input: ['http://leaderboard.crossfit.com/leaderboard.php?stage=3&sort=0&did=1&rid=1&npp=100&p=0&dd=1'],
+  run: function (leaderboardUrl) {
+    this.getHtml(leaderboardUrl, function(err, $) {
       if (err) this.exit(err);
 
       var athletes = [];
 
-      var that = this;
       // select all the athletes on the page
-      $('td.name a').each(function(a) {
+      $('div.leaderboard-box tr').has('td.number').each(function(tr) {
+        var rank = $('td.number', tr).text;
+        var athleteUrl = $('td.name a', tr).attribs.href;
         athletes.push({
-          "name": a.text,
-          "url": a.href
+          "rank": rank,
+          "athleteUrl": athleteUrl
         });
-        that.emit(a.text + ' ' + a.attribs.href);
       });
-
+      this.emit(athletes);
     });
   }
 });
