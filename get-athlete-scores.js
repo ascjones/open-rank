08bae4777f70a57ce4bf360cee680167c2fef30c
Andrew Jones <ascjones@gmail.com>
2012-03-12 09:54:21 +0200

    added mongo model for persisting athlete

--- get-athlete-scores.js
+++ get-athlete-scores.js
@@ -1,34 +1,63 @@
 var nodeio = require('node.io');
+var mongoose = require('mongoose');
+
+mongoose.createConnection('mongodb://localhost/open-rank');
+
+var AthleteSchema = new mongoose.Schema({
+  name : { type: String, required: true },
+  athleteId: { type: Number, required: true },
+  regionRank : { type: Number },
+  regionScore : { type: Number },
+  week1Rank : { type: Number },
+  week1Score : { type: Number },
+  week2Rank : { type: Number },
+  week2Score : { type: Number },
+  week3Rank : { type: Number },
+  week3Score : { type: Number },
+  week4Rank : { type: Number },
+  week4Score : { type: Number },
+  week5Rank : { type: Number },
+  week5Score : { type: Number }
+});
+
+var Athlete = mongoose.model('Athlete', AthleteSchema);
+
 exports.job = new nodeio.Job({
   run: function (leaderboardUrl) {
     this.getHtml(leaderboardUrl, function(err, $) {
       if (err) this.exit(err);
 
       var athletes = [];
-      var parseAthleteId = /\d+/;
       var parseRankAndScore = /(\d+)\s\((\d+)\)/; 
+      var parseAthleteId = /(\d+)/;
       $('div.leaderboard-box tr').has('td.number').each(function(tr) {
         var athleteLink = $('td.name a', tr);
         var overall = $('td.number', tr).text;
+        var overallRankAndScore = parseRankAndScore.exec(overall);
         var workoutScores = [];
         $('td span.display', tr).each(function(span) {
           var rankAndScore = parseRankAndScore.exec(span.text);
           if (rankAndScore) {
-            workoutScores.push({score: rankAndScore[1], rank: rankAndScore[2]});
+            workoutScores.push({rank: rankAndScore[1], score: rankAndScore[2]});
           }
         });
-        var overallRankAndScore = parseRankAndScore.exec(overall);
         if (overallRankAndScore) {
-          athletes.push({
-            id: parseAthleteId.exec(athleteLink.attribs.href)[0],
+          athletes.push(new Athlete({
             name: athleteLink.text,
-            overallRank: overallRankAndScore[1],
-            overallScore: overallRankAndScore[2],
-            workoutScores: workoutScores
-          });
+            athleteId: parseAthleteId.exec(athleteLink.attribs.href),
+            regionRank: overallRankAndScore[1],
+            regionScore: overallRankAndScore[2],
+            week1Rank: workoutScores[0].rank,
+            week1Score: workoutScores[0].score,
+          }));
         }
       });
       this.emit(athletes);
+    });
+  },
+  output: function(output) {
+    output.forEach(function(athlete) {
+      console.log(athlete);
     });
   }
 });
