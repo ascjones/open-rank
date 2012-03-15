337f98b58c6a57f9a380884c75248ea53a1d6529
Andrew Jones <ascjones@gmail.com>
2012-03-15 10:28:36 +0200

    assign athletes to their affiliates

--- get-affiliate-athletes.js
+++ get-affiliate-athletes.js
@@ -1,7 +1,12 @@
 var nodeio = require('node.io');
+var Mongolian = require('mongolian');
+
+var db = new Mongolian('localhost').db('open-rank');
+
 exports.job = new nodeio.Job({
-  input: ['http://games.crossfit.com/affiliate/2132'],  
-  run: function (affiliateUrl) {
+  input: [2132], // just CapeCrossfit for now  
+  run: function (affiliateId) {
+    var affiliateUrl = 'http://games.crossfit.com/affiliate/' + affiliateId;
     var parseAthleteId = /\d+/;
     this.getHtml(affiliateUrl, function(err, $) {
       if (err) this.exit(err);
@@ -9,9 +14,16 @@ exports.job = new nodeio.Job({
       $('section.block-affiliate-athletes ul li').each(function(li) {
         var athleteLink = $('a', li).first().attribs.href;
         var athleteId = parseAthleteId.exec(athleteLink)[0];
-        athletes.push(athleteId);
+        athletes.push({athleteId: athleteId, affiliateId: affiliateId});
       });     
       this.emit(athletes);
     })
+  },
+  output: function(output) {
+    var athletes = db.collection('athletes');
+    output.forEach(function(athlete) {
+      console.log('assigning athlete ' + athlete.athleteId + ' to affiliate ' + athlete.affiliateId);
+      athletes.update({athleteId: athlete.athleteId}, { $set: {affiliateId: athlete.affiliateId } });
+    });
   }
 })
