5bb1cca1088a63834a13cd1ca23843a4037d1cc7
Andrew Jones <ascjones@gmail.com>
2012-03-23 08:54:08 +0200

    update url for new larboard and include stage

--- scraping/get-leaderboard-pages.js
+++ scraping/get-leaderboard-pages.js
@@ -1,9 +1,9 @@
 var nodeio = require('node.io');
 exports.job = new nodeio.Job({
-  input: false,
-  run: function () {
+  input: [4],
+  run: function (stage) {
     var leaderboardRootUrl = "http://games.crossfit.com";
-    var firstLeaderboardPage = leaderboardRootUrl + "/scores/leaderboard.php?stage=3&sort=0&did=1&rid=1&npp=60&p=0&dd=1";
+    var firstLeaderboardPage = leaderboardRootUrl + "/scores/leaderboard.php?stage=" + stage + "&sort=0&did=1&rid=1&npp=60&p=0&dd=1";
     this.getHtml(firstLeaderboardPage, function(err, $) {
       if (err) this.exit(err);
       var leaderboardPages = [firstLeaderboardPage];
