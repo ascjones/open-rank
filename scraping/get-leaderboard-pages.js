bec30fedb0b395d4fea38b4a969edca2e60fe54f
Andrew Jones <ascjones@gmail.com>
2012-03-30 18:38:00 +0200

    changed to stage 5 for leaderboard

--- scraping/get-leaderboard-pages.js
+++ scraping/get-leaderboard-pages.js
@@ -1,6 +1,6 @@
 var nodeio = require('node.io');
 exports.job = new nodeio.Job({
-  input: [4],
+  input: [5],
   run: function (stage) {
     var leaderboardRootUrl = "http://games.crossfit.com";
     var firstLeaderboardPage = leaderboardRootUrl + "/scores/leaderboard.php?stage=" + stage + "&sort=0&did=1&rid=1&npp=60&p=0&dd=1";
